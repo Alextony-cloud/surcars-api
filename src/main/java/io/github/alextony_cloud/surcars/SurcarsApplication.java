08141675b571e4dd22eff5984170cb6d8f3120db
Alextony-cloud <alextonyrodrigues@gmail.com>
2024-06-10 11:42:18 -0300

    :tada: Commit inicial

---
+++ src/main/java/io/github/alextony_cloud/surcars/SurcarsApplication.java
@@ -0,0 +1,13 @@
+package io.github.alextony_cloud.surcars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurcarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurcarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/alextony_cloud/surcars/SurcarsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.alextony_cloud.surcars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurcarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
